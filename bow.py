3e1ca807856f8b91555357ccfdc16ed6e8117276
mikrivorot <m.i.krivorot@gmail.com>
2025-07-18 18:39:59 +0200

    add example for n-grams, print vocabulary

--- bow.py
+++ bow.py
@@ -47,5 +47,8 @@ X=cv.fit_transform(corpus).toarray()
 np.set_printoptions(edgeitems=30, linewidth=100000, 
     formatter=dict(float=lambda x: "%.3g" % x))
 print(X)
-
 # [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 0 0 0 ...
+
+
+print(cv.vocabulary_)
+# {'go': 23, 'great': 27, 'got': 26, 'wat': 90, 'ok': 59, 'free': 19, 'win': 94, 'text': 79, 'txt': 86, 'say': 70, 'already': 0, 'think': 82, 'life': 39, 'hey': 30, 'week': 92, 'back': 6, 'like': 40, 'still': 75, 'send': 72, 'friend': 20, 'prize': 65, 'claim': 10, 'call': 7, 'mobile': 50, 'co': 11, 'home': 32, 'want': 89, 'today': 84, 'cash': 9, 'day': 15, 'reply': 67, 'www': 96, 'right': 68, 'take': 77, 'time': 83, 'message': 47, 'com': 12, 'oh': 58, 'yes': 99, 'make': 45, 'way': 91, 'dont': 17, 'miss': 49, 'ur': 88, 'going': 24, 'da': 14, 'lor': 42, 'meet': 46, 'really': 66, 'know': 35, 'lol': 41, 'love': 43, 'amp': 2, 'let': 38, 'work': 95, 'yeah': 97, 'tell': 78, 'anything': 3, 'thanks': 80, 'uk': 87, 'please': 63, 'msg': 52, 'see': 71, 'pls': 64, 'need': 54, 'tomorrow': 85, 'hope': 33, 'well': 93, 'lt': 44, 'gt': 28, 'get': 21, 'ask': 4, 'morning': 51, 'happy': 29, 'sorry': 74, 'give': 22, 'new': 55, 'find': 18, 'year': 98, 'later': 37, 'pick': 62, 'good': 25, 'come': 13, 'said': 69, 'hi': 31, 'babe': 5, 'im': 34, 'much': 53, 'stop': 76, 'one': 60, 'night': 56, 'service': 73, 'dear': 16, 'thing': 81, 'last': 36, 'min': 48, 'number': 57, 'also': 1, 'care': 8, 'phone': 61}
